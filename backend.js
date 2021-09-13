135129c5eb787a036bf3b67a9a970954a2d7927d
Igor Rybak <igor@short.io>
2021-09-13 14:32:43 +0300

    add eslint backend rules

---
+++ backend.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+        commonjs: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:security/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "security"],
+    rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/no-empty-interface": 0,
+    },
+};
