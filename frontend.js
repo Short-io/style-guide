d649cb08eb19b4cb0fcfae87e98f16a0ee34366f
nrjinua <semylit.official@gmail.com>
2021-10-11 16:36:24 +0300

    add frontend configurations files for eslint

--- frontend.js
+++ frontend.js
@@ -1,1 +1,69 @@
-module.exports = {};
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "serviceworker": true,
+        "jest": true,
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "globals": {
+        "Clipboard": true,
+        "process": true,
+        "Stripe": true,
+        "RequestInit": true,
+        "JSX": true,
+    },
+    "settings": {
+        "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+        "polyfills": ["fetch", "url"],
+        "react": {
+            "version": "detect",
+        },
+    },
+    "plugins": ["@typescript-eslint", "react", "react-hooks", "jest"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:jest/recommended",
+    ],
+    "rules": {
+        "react/destructuring-assignment": [2, "always"],
+        "react/no-array-index-key": 2,
+        "react/no-multi-comp": 2,
+        "react/no-unstable-nested-components": 2,
+        "react/self-closing-comp": [2, {
+            "component": true,
+            "html": true
+        }],
+        "react/jsx-no-useless-fragment": 2,
+        "react/jsx-filename-extension": ["error", {
+            "extensions": [".tsx", ".jsx"]
+        }],
+        "@typescript-eslint/naming-convention": [2, {
+            "selector": "variable",
+            "format": ["camelCase", "UPPER_CASE", "PascalCase"]
+        }],
+        "@typescript-eslint/no-unused-vars": 2,
+        "@typescript-eslint/ban-ts-comment": [2, {
+            "ts-expect-error": "allow-with-description",
+        }],
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/ban-ts-ignore": 0,
+        "prefer-template": 2,
+        "no-var": 2,
+        "linebreak-style": [2, 'unix'],
+    },
+};
